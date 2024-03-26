b6c27cc421bad53c50ce5ed25cb5f19761b63f59
MykutaSuprun <162371997+MykutaSuprun@users.noreply.github.com>
2024-03-26 18:00:59 +0200

    Update task_2.py

--- lection 13/task_2.py
+++ lection 13/task_2.py
@@ -1,9 +1,9 @@
 class Country:
-    def init(self, name, population):
+    def __init__(self, name, population):
         self.name = name
         self.population = population
 
-    def add(self, other_country):
+    def __add__(self, other_country):
         combined_name = f"{self.name} {other_country.name}"
         combined_population = self.population + other_country.population
         return Country(combined_name, combined_population)
